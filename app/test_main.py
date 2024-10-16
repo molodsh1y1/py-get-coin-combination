f1a21f9812db61bb0eb70442d607347051049d96
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-10-17 02:09:07 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,19 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2])
+    ]
+)
+def test_calculate_coins_should_return_expected_amount_in_cents(
+        test_input: int,
+        expected: list[int]
+) -> None:
+    assert get_coin_combination(test_input) == expected
